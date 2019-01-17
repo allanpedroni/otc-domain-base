17b48d59ff99f295c1b2c998e0e9a6c9d1299adb
Matheus Neder <matheusneder@gmail.com>
2019-01-17 14:26:26 -0200

    Ajuste prop key para versao 2.1

--- Source/Otc.DomainBase.Exceptions/CoreException.cs
+++ Source/Otc.DomainBase.Exceptions/CoreException.cs
@@ -5,34 +5,34 @@ namespace Otc.DomainBase.Exceptions
 {
     public abstract class CoreException : Exception
     {
-        public CoreException(string message)
+        protected CoreException(string message)
             : base(message)
         {
 
         }
 
-        [Obsolete("Utilize a propriedade Key.")]
-        public virtual string TypeName => Key;
+        [Obsolete("Utilize a propriedade Key. Na proxima versao esta propriedade pode deixar de existir.")]
+        public virtual string TypeName => GetType().Name;
 
-        public abstract string Key { get; }
+        public virtual string Key => TypeName;
     }
 
     public abstract class CoreException<T> : CoreException
         where T : CoreError
     {
-        public CoreException()
+        protected CoreException()
             : base("Ocorreu um erro de negócio, verifique a propriedade 'errors' para obter detalhes.")
         {
 
         }
 
-        public CoreException(string message)
+        protected CoreException(string message)
             : base(message)
         {
 
         }
 
-        public ICollection<T> Errors = new List<T>();
+        public ICollection<T> Errors { get; } = new List<T>();
 
         public CoreException<T> AddError(params T[] errors)
         {
